22b7adfbb3db6097d75d39f8ddf3d5a47a222311
Yaoik <101880910+Yaoik@users.noreply.github.com>
2024-11-05 22:39:33 +0700

    Change Apps installed

--- IT_Solutions_cars/settings.py
+++ IT_Solutions_cars/settings.py
@@ -21,17 +21,25 @@ MY_APPS = [
     'users',
     'common',
 ]
+
 EXTERNAL_LIBRARIES = [
     'rest_framework',
 ]
-INSTALLED_APPS = [
+
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-] + MY_APPS + EXTERNAL_LIBRARIES
+]
+
+INSTALLED_APPS = [
+    *DJANGO_APPS,
+    *EXTERNAL_LIBRARIES,
+    *MY_APPS,
+]
 
 
 
