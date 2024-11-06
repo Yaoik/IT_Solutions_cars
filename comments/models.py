3ccd5d30de7c8affd4d1336bab8ea8ab0290c8c4
Yaoik <101880910+Yaoik@users.noreply.github.com>
2024-11-06 23:10:21 +0700

    Create Comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from common.models import Timestamped
+from users.models import User
+from cars.models import Car
 
-# Create your models here.
+
+
+class Comment(Timestamped):
+    """Комментарии к автомобилю"""
+    content = models.TextField()
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return f'<Comment к {self.car.model}>'
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
