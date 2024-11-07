4c403326ab78e101621f5aca459a9e4d17887ba5
Yaoik <101880910+Yaoik@users.noreply.github.com>
2024-11-07 22:26:39 +0700

    Add comment create form

---
+++ static/js/comments.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+    url = $('input[name=comments_url]').val()
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        headers: {
+            'Authorization': localStorage.getItem('Token')
+        },
+        success: function(response) {
+            $('#comment-list').empty();
+            $.each(response, function(index, comment) {
+                var commentHtml = `<div class="min-w-64 border">`;
+                commentHtml += '<h3>' + comment.author ;
+                commentHtml += '<p><strong>Создан:</strong> ' + comment.created_at + '</p>';
+                commentHtml += '<p><strong>Текст:</strong> ' + comment.content + '</p>';
+                commentHtml += '</div>';
+
+                $('#comment-list').append(commentHtml);
+            });
+        },
+        error: function(xhr) {
+            console.log('(')
+        }
+    });
+    $('#create_comment').click(function(e) {
+        e.preventDefault();
+
+        var csrftoken = $('input[name="csrfmiddlewaretoken"]').val();
+
+        var formData = {
+            content: $('#content').val(),
+        };
+        for (i in formData) {
+            if (formData[i] == ''){delete formData[i]}
+        }
+        $.ajax({
+            url: url,
+            type: 'POST',
+            dataType: 'json',
+            data: JSON.stringify(formData),
+            contentType: 'application/json',
+            headers: {
+                'X-CSRFToken': csrftoken,
+            },
+            success: function(response) {
+                window.location.reload()
+            },
+            error: function(xhr) {
+                var errors;
+                try {
+                    errors = JSON.parse(xhr.responseText);
+                } catch (e) {
+                    errors = xhr.responseText;
+                }
+                var errorMessages = '';
+                if (typeof errors === 'string') {
+                    errorMessages += '<p style="color: red;">' + errors + '</p>';
+                }
+                else if (typeof errors === 'object' && errors !== null) {
+                    $.each(errors, function(field, messages) {
+                        if (Array.isArray(messages)) {
+                            $.each(messages, function(index, message) {
+                                errorMessages += '<p style="color: red;">Ошибка: ' + field + ' -> ' + message + '</p>';
+                            });
+                        } else {
+                            errorMessages += '<p style="color: red;">Ошибка: ' + field + ' -> ' + messages + '</p>';
+                        }
+                    });
+                }
+                $('#responseMessage_comment').html(errorMessages);
+            }
+        });
+    });
+});
